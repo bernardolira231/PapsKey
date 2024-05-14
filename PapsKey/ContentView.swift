dbcfe92231166c725199ab6dd6c45b57d3612e72
Bernardo Lira <harritolira@gmail.com>
2024-05-14 12:44:55 -0600

    Initial Commit

---
+++ PapsKey/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PapsKey
+//
+//  Created by Bernardo Lira on 14/05/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PapsKey/PapsKeyApp.swift
@@ -0,0 +1,17 @@
+//
+//  PapsKeyApp.swift
+//  PapsKey
+//
+//  Created by Bernardo Lira on 14/05/24.
+//
+
+import SwiftUI
+
+@main
+struct PapsKeyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
